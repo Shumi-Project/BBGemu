4e3785b013d0a8057fc14588e8efc38e011b1d68
AozoraDev <niicoremusic@gmail.com>
2023-06-14 11:03:03 +0800

    Uhh.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,18 @@ function hideLoading() {
     header.style.animation = "fade 1s";
 }
 
+function toDate(value) {
+    const date = new Date(value);
+    
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    
+    const formattedDate = ('0' + day).slice(-2) + '-' + ('0' + month).slice(-2) + '-' + year;
+    
+    return formattedDate;
+}
+
 fetch("https://api.aozora.my.id/bbgemu/playlists")
 .then(res => res.json())
 .then(res => {
@@ -23,7 +35,7 @@ fetch("https://api.aozora.my.id/bbgemu/playlists")
         let list = document.createElement("div");
         let a = document.createElement("a");
         list.classList.add("list");
-        a.href = "#";
+        a.href = "https://youtube.com/playlist?list=" + item.id;
         
         let img = document.createElement("img");
         img.src = item.thumbnails.maxres.url;
@@ -33,6 +45,7 @@ fetch("https://api.aozora.my.id/bbgemu/playlists")
         let info = document.createElement("div");
         info.classList.add("info");
         info.innerHTML = `<h3><b>${item.title}</b></h3>`;
+        info.innerHTML += `<div class="data"><i class="bx bx-calendar-event"></i><span>${toDate(item.publishedAt)}</span><vl></vl><i class="bx bx-movie-play"></i><span>${item.itemCount}</span></div>`
         a.appendChild(info);
         
         list.appendChild(a);
